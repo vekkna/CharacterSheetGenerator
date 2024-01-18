734c3d47775b60836436aee0c25c5b9ec3776710
Neil Crowley <neilccrowley@gmail.com>
2024-01-18 12:03:45 +0000

    Add files via upload

---
+++ code/code.js
@@ -0,0 +1,5 @@
+function createCharacter(){
+    //Find the input field containing the name (use its id)
+    //store its value in a variable
+    //put that name into the p in the character sheet (find the p by its id)
+}
